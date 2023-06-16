8cf54c8117a01b625dbf03899936bd75c7ef75db
tingul4 <40429714+tingul4@users.noreply.github.com>
2023-06-16 05:32:04 -0400

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
 const express = require('express')
-const app = express()
 const port = 3000
+
+// 
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+const app = express()
+
+const mongoose = require('mongoose')
+mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+
+// 取得資料庫連線狀態
+const db = mongoose.connection
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 app.get('/', (req, res) => {
   res.send('It\'s a to-do list.')
