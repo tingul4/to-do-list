24b22445c369381be92c235347a5d0a2e1f3ed1b
tingul4 <40429714+tingul4@users.noreply.github.com>
2023-06-16 05:54:28 -0400

    feat: add todo seeder

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ if (process.env.NODE_ENV !== 'production') {
 const app = express()
 
 const mongoose = require('mongoose')
-mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
 // 取得資料庫連線狀態
 const db = mongoose.connection
