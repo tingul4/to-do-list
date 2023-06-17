0d600dcc268643833444df83dd6e3420adc94776
tingul4 <40429714+tingul4@users.noreply.github.com>
2023-06-17 11:51:37 -0400

    feat: add delete todo function

--- app.js
+++ app.js
@@ -91,6 +91,15 @@ app.post('/todos/:id/edit', (req, res) => {
           .catch(error => console.error(error))
 })
 
+app.post('/todos/:id/delete', (req, res) => {
+  const id = req.params.id
+
+  return Todo.findById(id)
+          .then(todo => todo.remove())
+          .then(() => res.redirect('/'))
+          .catch(error => console.error(error))
+})
+
 app.listen(port, () => {
   console.log(`Express is listening on localhost:${port}`)
 })
