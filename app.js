86f955f6e1f1f93db59cb9724442fd610322c279
tingul4 <40429714+tingul4@users.noreply.github.com>
2023-06-17 11:33:23 -0400

    feat: add update todo function

--- app.js
+++ app.js
@@ -61,9 +61,34 @@ app.post('/todos', (req, res) => {
 
 app.get('/todos/:id', (req, res) => {
   const id = req.params.id
+
   return Todo.findById(id)
           .lean()
           .then(todo => res.render('details', { todo }))
+          .catch(error => console.error(error))
+})
+
+app.get('/todos/:id/edit', (req, res) => {
+  const id = req.params.id
+  const name = req.body.name
+
+  return Todo.findById(id)
+          .lean()
+          .then(todo => res.render('edit', { todo }))
+          .catch(error => console.error(error))
+})
+
+app.post('/todos/:id/edit', (req, res) => {
+  const id = req.params.id
+  const name = req.body.name
+
+  return Todo.findById(id)
+          .then(todo => {
+            todo.name = name
+            return todo.save()
+          })
+          .then(() => res.redirect(`/todos/${id}`))
+          .catch(error => console.error(error))
 })
 
 app.listen(port, () => {
