83cbd3884a348558044fbe8e0865b86a5674ec99
tingul4 <40429714+tingul4@users.noreply.github.com>
2023-06-20 23:27:00 -0400

    chore: heroku init

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const methodOverride = require('method-override')
 
 const routes = require('./routes') // 引用路由器
 require('./config/mongoose')
-const port = 3000
+const PORT = process.env.PORT || 3000
 
 const app = express()
 
@@ -17,6 +17,6 @@ app.use(bodyParser.urlencoded({ extended : true }))
 app.use(methodOverride('_method')) // 設定每一筆請求都會透過 methodOverride 進行前置處理
 app.use(routes) // 將 request 導入路由器
 
-app.listen(port, () => {
-  console.log(`Express is listening on localhost:${port}`)
+app.listen(PORT, () => {
+  console.log(`Express is listening on localhost:${PORT}`)
 })
