323bd00becc693ea6f35e95b13ad080bd8eaa47b
tingul4 <40429714+tingul4@users.noreply.github.com>
2023-07-13 17:14:18 -0400

    feat: add user modules

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,11 @@ const router = express.Router()
 
 const home = require('./modules/home')
 const todos = require('./modules/todos')
+const users = require('./modules/users')
 
 // 將網址結構符合 / 字串的 request 導向 home 模組 
 router.use('/', home)
 router.use('/todos', todos)
+router.use('/users', users)
 
 module.exports = router
