e775e0f4b78370366d71109d755347c2d85212a0
tingul4 <40429714+tingul4@users.noreply.github.com>
2023-07-16 00:16:45 -0400

    modify todoSeeder

--- models/seeds/todoSeeder.js
+++ models/seeds/todoSeeder.js
@@ -1,11 +1,33 @@
-const Todo = require('../todo') // 載入 todo model
+const bcrypt = require('bcryptjs')
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+const Todo = require('../todo')
+const User = require('../user')
 const db = require('../../config/mongoose')
-
-// create 10 of data
+const SEED_USER = {
+  name: 'root',
+  email: 'root@example.com',
+  password: '12345678'
+}
 db.once('open', () => {
-  for (let i = 0; i < 10; i++) {
-    Todo.create({name:`name-${i}`})
-  }
-  
-  console.log('done')
+  bcrypt
+    .genSalt(10)
+    .then(salt => bcrypt.hash(SEED_USER.password, salt))
+    .then(hash => User.create({
+      name: SEED_USER.name,
+      email: SEED_USER.email,
+      password: hash
+    }))
+    .then(user => {
+      const userId = user._id
+      return Promise.all(Array.from(
+        { length: 10 },
+        (_, i) => Todo.create({ name: `name-${i}`, userId })
+      ))
+    })
+    .then(() => {
+      console.log('done.')
+      process.exit()
+    })
 })
