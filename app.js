184abd1ab17f44b3db8d8245f8cba820e27614c3
tingul4 <40429714+tingul4@users.noreply.github.com>
2023-07-15 21:58:41 -0400

    feat: add message partials

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser') // use bodyParser to get post form data correctly
 const methodOverride = require('method-override')
 const session = require('express-session')
+const flash = require('connect-flash')
 
 const routes = require('./routes') // 引用路由器
 const usePassport = require('./config/passport')
@@ -25,9 +26,12 @@ app.use(bodyParser.urlencoded({ extended : true }))
 app.use(methodOverride('_method')) // 設定每一筆請求都會透過 methodOverride 進行前置處理
 
 usePassport(app)
+app.use(flash())
 app.use((req, res, next) => {
   res.locals.isAuthenticated = req.isAuthenticated()
   res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')  // 設定 success_msg 訊息
+  res.locals.warning_msg = req.flash('warning_msg')  // 設定 warning_msg 訊息
   next()
 })
 app.use(routes) // 將 request 導入路由器
